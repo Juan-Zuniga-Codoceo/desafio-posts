e9db5938ceaa7575d6549322ed0bcbf09ebca6b4
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2024-04-02 18:28:58 -0300

    3

--- desafioI.js
+++ desafioI.js
@@ -1,34 +1,20 @@
 async function getPosts() {
     try {
-        
         const respuesta = await fetch('https://jsonplaceholder.typicode.com/posts');
-        
-        
         if (!respuesta.ok) {
             throw new Error(`Error HTTP: ${respuesta.status}`);
         }
         
-        
         const posts = await respuesta.json();
-        
-        
         const div = document.querySelector('#post-data');
-        
-        
         div.innerHTML = '';
-        
-       
         const lista = document.createElement('ul');
         div.appendChild(lista);
         
-        
         for (let post of posts) {
             const item = document.createElement('li');
-            
             item.innerHTML = `<strong>${post.title}</strong><br>${post.body}`;
-            
             lista.appendChild(item);
-            
             const lineBreak = document.createElement('br');
             lista.appendChild(lineBreak);
         }
